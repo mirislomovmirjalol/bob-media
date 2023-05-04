a5e17f7516f2e311c7937ecb19fc2bf2b1e3ccac
mirjalol <mirislomovmirjalol@gmail.com>
2023-05-04 11:36:34 +0100

    main page, feature films page with connection to nuxt content

---
+++ pages/feature-films/[...slug].vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const title = "text";
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <BackButton :title="title"/>
+    <div class="container mx-auto p-4">
+      <div class="md:w-3/4 mx-auto">
+        <ContentDoc/>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/feature-films/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen">
+    <Navbar/>
+    <div class="container mx-auto p-4">
+      <ContentList path="/feature-films">
+        <template #default="{ list }">
+          <NuxtLink v-for="item in list" :to="item._path" class="hover:text-yellow-500 cursor-pointer">
+            <h3 v-text="item.title"/>
+          </NuxtLink>
+        </template>
+        <template #not-found>
+          <p>No articles found.</p>
+        </template>
+      </ContentList>
+    </div>
+  </div>
+</template>
