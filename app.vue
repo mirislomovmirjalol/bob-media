b36a9d2f8e478e25b708134f8ba65d942235e135
mirjalol <mirislomovmirjalol@gmail.com>
2023-05-04 10:01:53 +0100

    first page with nuxt content

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+  <div class="text-white bg-black">
     <NuxtPage />
   </div>
 </template>
+
+<style>
+h1 {
+  @apply text-4xl;
+}
+p {
+  @apply text-sm text-gray-300;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        Hello my bro
+  <div class="min-h-screen">
+    <Navbar/>
+    <div class="container mx-auto p-4">
+      <ContentDoc/>
     </div>
+  </div>
 </template>
